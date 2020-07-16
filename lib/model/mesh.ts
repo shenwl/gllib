6897285ac84f87f4d0afa622b702cbebb309885a
shenwl <shenweilun96@gmail.com>
2020-07-16 20:53:17 +0800

    重构Mesh

--- lib/model/mesh.ts
+++ lib/model/mesh.ts
@@ -1,63 +1,69 @@
 import RenderContext from '../renderContext';
+import { GLVertexBuffer } from './glVertexBuffer';
+import { GLIndexBuffer } from './glIndexBuffer';
 
 interface IProps {
   vertexes: number[];
-  indexes?: any;
+  indices?: number[];
   dimension?: number;
+  colors?: number[];
 }
 
 export class Mesh {
-  dimension: number;
+  indices: number[];
   vertexes: number[];
-  indexes: any;
+  dimension: number;
   gl: WebGLRenderingContext;
   program: WebGLProgram;
   vertexPosition: GLint;
-  vertexBuffer: WebGLBuffer;
+  vertexBuffer: GLVertexBuffer;
+  colorsBuffer: GLVertexBuffer;
+  indicesBuffer: GLIndexBuffer;
 
   constructor(
-    { vertexes, indexes = null, dimension = 3 }: IProps
+    { vertexes, indices = null, dimension = 3, colors = null }: IProps
   ) {
     this.dimension = dimension;
+    this.indices = indices;
     this.vertexes = vertexes;
-    this.indexes = indexes;
-
     // webGL实例
     this.gl = RenderContext.getGL();
     this.program = RenderContext.getProgram();
+
     this.vertexPosition = this.gl.getAttribLocation(this.program, 'a_position');
     // 绘制网格需要buffer
-    this.vertexBuffer = null;
-    this.init();
-  }
+    this.vertexBuffer = new GLVertexBuffer('a_position', new Float32Array(vertexes), dimension);
 
-  init() {
-    // 创建缓冲区
-    this.vertexBuffer = this.gl.createBuffer();
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
-    // bufferData需要32位Float数组
-    this.gl.bufferData(
-      this.gl.ARRAY_BUFFER,
-      new Float32Array(this.vertexes),
-      this.gl.STATIC_DRAW,
-    );
+    if (colors) {
+      this.colorsBuffer = new GLVertexBuffer('a_color', new Float32Array(colors), dimension);
+    }
+    if (indices) {
+      this.indicesBuffer = new GLIndexBuffer(new Uint16Array(indices), dimension);
+    }
   }
 
   draw() {
-    this.gl.enableVertexAttribArray(this.vertexPosition);
-    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
-    const type = this.gl.FLOAT;
-    const normalized = false;
-    const stride = 0;
-    const offset = 0;
-    this.gl.vertexAttribPointer(
-      this.vertexPosition,
-      this.dimension,
-      type,
-      normalized,
-      stride,
-      offset,
+    const { gl } = this;
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    this.vertexBuffer.associate();
+    this.colorsBuffer && this.colorsBuffer.associate();
+    this.indicesBuffer && this.indicesBuffer.associate();
+
+    if (this.indicesBuffer) {
+      gl.drawElements(
+        gl.TRIANGLES,
+        this.indices.length,
+        gl.UNSIGNED_SHORT,
+        0,
+      );
+      return;
+    }
+    gl.drawArrays(
+      gl.TRIANGLES,
+      0,
+      this.vertexes.length / this.dimension,
     );
-    this.gl.drawArrays(this.gl.TRIANGLES, 0, this.vertexes.length / this.dimension);
   }
 }
