5c22282ad02cadad60d536b3989b68b8d24bbea3
shenwl <shenweilun96@gmail.com>
2020-07-17 22:17:37 +0800

    model export

---
+++ lib/model/index.ts
@@ -0,0 +1,12 @@
+import { Model } from './model';
+import { Mesh } from './mesh';
+import { GLIndexBuffer } from './glIndexBuffer';
+import { GLVertexBuffer } from './glVertexBuffer';
+
+
+export {
+  Model,
+  Mesh,
+  GLIndexBuffer,
+  GLVertexBuffer,
+}
