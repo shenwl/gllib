68860dba9a35856737726ff9618d3d14b3c218e8
shenwl <shenweilun96@gmail.com>
2020-10-21 21:10:28 +0800

    简化代码 & 测试Model添加材质功能

--- demo/texSphereWidthMesh/index.ts
+++ demo/texSphereWidthMesh/index.ts
@@ -1,8 +1,6 @@
 import { Shapes, Utils, Matrix4, Mesh, Model } from '../../lib/index';
 import texImage from './images/mon.jpg';
 
-const loadTexture = Utils.createTextureLoader();
-
 const vShader = `
   attribute vec4 a_Position;
   uniform mat4 u_MvpMatrix;
@@ -27,20 +25,6 @@ const fShader = `
   }
 `
 
-function initTexture(gl: WebGLRenderingContext, program: WebGLProgram, n: number, onLoad: () => void) {
-  const u_Texture = gl.getUniformLocation(program, 'u_Texture');
-  const img = new Image();
-
-  img.onload = function () {
-    const texture = gl.createTexture();
-    loadTexture(gl, n, texture, u_Texture, img, () => {
-      onLoad();
-    });
-  }
-
-  img.src = texImage;
-}
-
 function __main__() {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const gl = Utils.initGl(canvas);
@@ -52,23 +36,23 @@ function __main__() {
   const mesh = new Mesh({ gl, program, vertexes, indices, texCoords });
   const model = new Model(gl, program, mesh);
 
+  model.addTextureImage(texImage);
+
   let angle = 0;
-  
+
   const draw = () => {
     angle += 1;
 
     const mvpMatrix = new Matrix4();
     mvpMatrix.setPerspective(30, 1, 1, 100).lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0).rotate(angle, 0, 1, 0);
-    model.setMatrixUniform('u_MvpMatrix', mvpMatrix.elements)
 
+    model.setMatrixUniform('u_MvpMatrix', mvpMatrix.elements)
     model.draw(gl.TRIANGLE_STRIP);
 
     requestAnimationFrame(draw);
   };
 
-  initTexture(gl, program, indices.length, () => {
-    draw();
-  });
+  draw();
 }
 
 __main__();
