66986a5a419b08e0c2f54bcd1762b9a9837ecd85
shenwl <shenweilun96@gmail.com>
2020-11-01 20:39:27 +0800

    简单的点光立方体

--- demo/cubePointLight/index.ts
+++ demo/cubePointLight/index.ts
@@ -2,46 +2,46 @@ import { Shapes, Utils, Matrix4, Model, Mesh } from '../../lib/index';
 
 const vShader = `
   attribute vec4 a_Position;
-  attribute vec4 a_Color;
   attribute vec3 a_Normal;
 
   uniform mat4 u_World;
-  uniform mat4 u_MvpMatrix;
+  uniform mat4 u_Unit;
+  uniform vec3 u_Light;
+  uniform vec3 u_Camera;
 
-  varying vec4 v_Color;
   varying vec3 v_Norm;
+  varying vec3 v_SurfaceToCamera;
+  varying vec3 v_SurfaceToLight;
 
   void main() {
-    gl_Position = u_MvpMatrix * u_World * a_Position;
-    v_Color = a_Color;
-    v_Norm = mat3(u_World) * a_Normal;
+    vec3 surface = (u_World * a_Position).xyz;
+    gl_Position = u_World * u_Unit * a_Position;
+    v_Norm = mat3(u_World * u_Unit) * a_Normal;
+    v_SurfaceToCamera = u_Light - surface;
+    v_SurfaceToCamera = u_Camera - surface;
   }
 `
 
 const fShader = `
   precision mediump float;
-  uniform vec3 u_Light; // 光照方向
 
   varying vec4 v_Color;
   varying vec3 v_Norm;
+  varying vec3 v_SurfaceToCamera;
+  varying vec3 v_SurfaceToLight;
 
   void main() {
     vec3 normal = normalize(v_Norm);
-    vec3 lightDir = normalize(u_Light);
-    float light = dot(normal, -lightDir);
-    gl_FragColor = v_Color;
+    vec3 halfVec = normalize(v_SurfaceToCamera + v_SurfaceToLight);
+    vec3 surfaceToLight = normalize(halfVec);
+    float light = dot(normal, surfaceToLight);
+    float specular = dot(normal, halfVec);
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
     gl_FragColor.rgb *= light;
+    gl_FragColor.rgb += specular;
+    gl_FragColor += pow(specular, 100.0);
   }
 `
-
-const colors = [
-  0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1,
-  0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4,
-  1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4,
-  1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4,
-  0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1,
-  1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1,
-];
 
 function __main__() {
   const light = { x: 10, y: 100, z: -100 };
@@ -52,19 +52,14 @@ function __main__() {
   const Cube = Shapes.d3Cube;
   const { vertexes, indices, norms } = Cube;
 
-  const model = new Model(gl, program, new Mesh({ gl, program, vertexes, indices, colors, norms }));
-
-  const mvpMatrix = new Matrix4();
-  mvpMatrix.setPerspective(30, 1, 1, 100).lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0);
-  model.setMatrixUniform('u_MvpMatrix', mvpMatrix.elements);
+  const model = new Model(gl, program, new Mesh({ gl, program, vertexes, indices, norms }));
 
   let angle = 0;
 
   const draw = () => {
     angle += 1;
 
-
-    model.setWorldMatrix(new Matrix4().rotate(angle, 0, 1, 0));
+    model.setWorldMatrix(new Matrix4().setPerspective(30, 1, 1, 100).lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0).rotate(angle, 0, 1, 0));
     model.setVectorUniform('u_Light', new Float32Array([light.x, light.y, light.z]));
     model.updateMatrix();
 
