c28b3e4ca314ef7505a442620f15f60abe47ed2f
shenwl <shenweilun96@gmail.com>
2020-11-01 20:32:48 +0800

    调整光照逻辑

--- demo/cubeSunlight/index.ts
+++ demo/cubeSunlight/index.ts
@@ -2,19 +2,16 @@ import { Shapes, Utils, Matrix4, Model, Mesh } from '../../lib/index';
 
 const vShader = `
   attribute vec4 a_Position;
-  attribute vec4 a_Color;
   attribute vec3 a_Normal;
 
   uniform mat4 u_World;
-  uniform mat4 u_MvpMatrix;
+  uniform mat4 u_Unit;
 
-  varying vec4 v_Color;
   varying vec3 v_Norm;
 
   void main() {
-    gl_Position = u_MvpMatrix * u_World * a_Position;
-    v_Color = a_Color;
-    v_Norm = mat3(u_World) * a_Normal;
+    gl_Position = u_World * u_Unit * a_Position;
+    v_Norm = mat3(u_World * u_Unit) * a_Normal;
   }
 `
 
@@ -22,26 +19,16 @@ const fShader = `
   precision mediump float;
   uniform vec3 u_Light; // 光照方向
 
-  varying vec4 v_Color;
   varying vec3 v_Norm;
 
   void main() {
     vec3 normal = normalize(v_Norm);
-    vec3 lightDir = normalize(u_Light);
-    float light = dot(normal, -lightDir);
-    gl_FragColor = v_Color;
+    float light = dot(normal, -u_Light);
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
     gl_FragColor.rgb *= light;
   }
 `
 
-const colors = [
-  0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1,
-  0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4,
-  1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4, 1, 0.4, 0.4,
-  1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4,
-  0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1,
-  1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1, 1, 0.4, 1,
-];
 
 function __main__() {
   const light = { x: 10, y: 100, z: -100 };
@@ -52,19 +39,16 @@ function __main__() {
   const Cube = Shapes.d3Cube;
   const { vertexes, indices, norms } = Cube;
 
-  const model = new Model(gl, program, new Mesh({ gl, program, vertexes, indices, colors, norms }));
+  const model = new Model(gl, program, new Mesh({ gl, program, vertexes, indices, norms }));
 
-  const mvpMatrix = new Matrix4();
-  mvpMatrix.setPerspective(30, 1, 1, 100).lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0);
-  model.setMatrixUniform('u_MvpMatrix', mvpMatrix.elements);
+  model.setWorldMatrix(new Matrix4().setPerspective(30, 1, 1, 100).lookAt(6, 6, 14, 0, 0, 0, 0, 1, 0));
 
   let angle = 0;
 
   const draw = () => {
     angle += 1;
 
-
-    model.setWorldMatrix(new Matrix4().rotate(angle, 0, 1, 0));
+    model.setUnitMatrix(new Matrix4().rotate(angle, 0, 1, 0))
     model.setVectorUniform('u_Light', new Float32Array([light.x, light.y, light.z]));
     model.updateMatrix();
 
